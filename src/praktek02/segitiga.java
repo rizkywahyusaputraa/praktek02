6566d860a729f842aeb3b5eb536d98de41c78e62
rizkywahyusaputraa <32368070+rizkywahyusaputraa@users.noreply.github.com>
2018-04-02 14:23:28 +0800

    membuat atribut dan behavior

--- src/praktek02/segitiga.java
+++ src/praktek02/segitiga.java
@@ -1,1 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package praktek02;
+
 public class segitiga {
+    double tinggi;
+    
+       System.out.println("=================");      
+       System.out.println("alas :"+alas);
+       System.out.println("tinggi :"+tinggi);
+       System.out.println("=================");
+    }
+    
+}
