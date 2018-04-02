b8696fff1ab15c0b9a77a9cc4177b1d13f501495
rizkywahyusaputraa <32368070+rizkywahyusaputraa@users.noreply.github.com>
2018-04-02 14:29:58 +0800

    mengisi nilai atribut

--- src/praktek02/segitigaaksi.java
+++ src/praktek02/segitigaaksi.java
@@ -5,6 +5,9 @@ package praktek02;
 public class segitigaaksi {
     public static void main(String[] args) {
         segitiga r1 = new segitiga ();
+        r1.alas = 5;
+        r1.tinggi = 6;
+        
     }
  
 }
